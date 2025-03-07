4422f1bfeaaa59828dec078647ec18e5f1cef50d
Uguls <chobozaex@gmail.com>
2025-03-07 11:10:16 +0900

    feat: lv2 완료
    객체 지향 설계를 적용해 햄버거 메뉴를 클래스로 관리

--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -8,13 +8,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<MenuItem> menuList = new ArrayList<>();
 
+        menuList.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         while (true) {
-            System.out.println("[ SHAKESHACK MENU ]\n" +
-                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                    "0. 종료      | 종료");
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i = 0; i < menuList.size(); i++) {
+                MenuItem menuItem = menuList.get(i);
+                System.out.println(i + 1 + ". " + menuItem.name + " | W " + menuItem.price + " | " + menuItem.descriptions);
+            }
+            System.out.println("0. 종료      | 종료");
             int menuInput = scanner.nextInt();
 
             switch (menuInput) {
@@ -22,10 +27,10 @@ public class Main {
                     System.out.println("프로그램을 종료합니다");
                     System.exit(0);
                 }
-                case 1 -> System.out.println("ShackBurger");
-                case 2 -> System.out.println("SmokeShack");
-                case 3 -> System.out.println("Cheeseburger");
-                case 4 -> System.out.println("Hamburger");
+                case 1 -> System.out.println("ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                case 2 -> System.out.println("SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                case 3 -> System.out.println("Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                case 4 -> System.out.println("Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             }
 
         }
