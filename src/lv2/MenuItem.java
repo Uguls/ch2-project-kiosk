8102658fab53772f7268429c9992c53929364d46
Uguls <chobozaex@gmail.com>
2025-03-07 10:43:21 +0900

    feat: MenuItem 클래스 추가
    개별 음식 항목을 관리하는 MenuItem클래스 추가

---
+++ src/lv2/MenuItem.java
@@ -0,0 +1,13 @@
+package lv2;
+
+public class MenuItem {
+    String name;
+    double price;
+    String descriptions;
+
+    public MenuItem(String name, double price, String descriptions) {
+        this.name = name;
+        this.price = price;
+        this.descriptions = descriptions;
+    }
+}
